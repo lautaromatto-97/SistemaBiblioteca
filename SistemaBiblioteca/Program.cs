66bd4c56fc93aa8ed2a8551a91fee9c3266e940f
lautaromatto-97 <lautaro.matto@mi.unc.edu.ar>
2026-06-03 21:00:15 -0300

    Creado menú principal y bienvenida al usuario

--- SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/Program.cs
@@ -1,4 +1,18 @@
-﻿Console.WriteLine("escriba su nombre");
+﻿Console.WriteLine("Escriba su nombre: ");
 string nombre = Console.ReadLine();
 Console.WriteLine($"hola {nombre}, bienvenido al sistema de biblioteca");
 
+Console.WriteLine("\nPresione cualquier tecla para ingresar al menu..");
+Console.ReadKey();
+
+Console.Clear();
+
+Console.WriteLine("====== SISTEMA BIBLIOTECA ======");
+Console.WriteLine("1. Agregar libro");
+Console.WriteLine("2. Mostra libros");
+Console.WriteLine("3. Salir");
+
+Console.WriteLine("\nSeleccione una opcion: ");
+string opcion = Console.ReadLine();
+
+Console.WriteLine($"Elegiste la opción {opcion}");
